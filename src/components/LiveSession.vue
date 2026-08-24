3cd3e711d14eeb11f09dd4965a38e6f090d2f8c1
Benoit <martel.b@gmail.com>
2026-08-24 12:31:43 -0400

    Fix template ref access for checkedSkillIds

--- src/components/LiveSession.vue
+++ src/components/LiveSession.vue
@@ -2395,7 +2395,7 @@ defineExpose({
         >
           <template v-if="hasEvalSelection">
             <Sneaker :size="20" />
-            <span class="fab-selected-name">{{ checkedSkillIds.value.size }}</span>
+            <span class="fab-selected-name">{{ checkedSkillIds.size }}</span>
           </template>
           <template v-else>
             <Sneaker :size="20" />
